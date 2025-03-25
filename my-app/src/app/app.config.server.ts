d393434a67f63d542e9125f630c480ff42ce79fa
houdaifaznagui <houdayfahouayfa7@gmail.com>
2025-03-25 09:22:14 +0000

    starting my first angular components

--- my-app/src/app/app.config.server.ts
+++ my-app/src/app/app.config.server.ts
@@ -1,7 +1,7 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-
+import { HelloWord } from './HelloWordComponents';
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering()
